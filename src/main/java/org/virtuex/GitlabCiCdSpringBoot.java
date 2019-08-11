3a3b79a48e394f29c63fd12b1e2e4d7b7cd6908e
xuda <xuda.it@outlook.com>
2019-08-11 15:05:12 +0800

    集成sonarquble

---
+++ src/main/java/org/virtuex/GitlabCiCdSpringBoot.java
@@ -0,0 +1,11 @@
+package org.virtuex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCiCdSpringBoot {
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabCiCdSpringBoot.class, args);
+    }
+}
