fdb80643595fe4c7e793f7f319379f3f84cc682d
xuda <xuda.it@outlook.com>
2019-08-15 23:28:50 +0800

    ci test

--- src/main/java/org/virtuex/GitlabCiCdSpringBoot.java
+++ src/main/java/org/virtuex/GitlabCiCdSpringBoot.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitlabCiCdSpringBoot {
+
     public static void main(String[] args) {
         SpringApplication.run(GitlabCiCdSpringBoot.class, args);
     }
