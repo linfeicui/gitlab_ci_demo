3c718b7c2463d0557a643edfa9763d4810d31999
xuda <xuda.it@outlook.com>
2019-08-16 21:53:59 +0800

    ci test

--- src/test/bin/create_database.sh
+++ src/test/bin/create_database.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-HOSTNAME="192.168.23.102"  #数据库信息
-PORT="3306"
-USERNAME="root"
-PASSWORD="root123"
-DBNAME="test_db_test"  #数据库名称
-TABLENAME="test_table_test" #数据库中表的名称
-#创建数据库
-create_db_sql="create database IF NOT EXISTS ${DBNAME}"
-mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} -e "${create_db_sql}"
+echo $CI_COMMIT_SHA
+#HOSTNAME="192.168.23.102"  #数据库信息
+#PORT="3306"
+#USERNAME="root"
+#PASSWORD="root123"
+#DBNAME="test_db_test"  #数据库名称
+#TABLENAME="test_table_test" #数据库中表的名称
+##创建数据库
+#create_db_sql="create database IF NOT EXISTS $CI_COMMIT_SHA"
+#mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} -e "${create_db_sql}"
 
